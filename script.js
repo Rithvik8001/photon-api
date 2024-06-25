f3a92803f1708e9c68797be6b3bf14d710f6e7ea
Rithvik8001 <rithvik.utd@outlook.com>
2024-06-24 20:37:32 -0700

    search image functionality

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const inputForm = document.querySelector(`.search-input`);
 const gallery = document.querySelector(`.gallery`);
 const submitBtn = document.querySelector(`.submit-btn`);
 
+let searchValue;
+// event listeners
+
+inputForm.addEventListener(`click`, (e) => {
+  e.preventDefault();
+  searchValue = e.target.value;
+});
+
 // funtions
 
 // function to get the data from the api
@@ -15,7 +23,7 @@ const curatedPhtos = async () => {
       Authorization: apiKey,
     },
   });
-  // pase the json file to object so we can work with it
+  // paste the json file to object so we can work with it
   const data = await dataFetch.json();
   data.photos.forEach((photo) => {
     const galleryImages = document.createElement(`div`);
@@ -25,5 +33,24 @@ const curatedPhtos = async () => {
     gallery.appendChild(galleryImages);
   });
 };
-
 curatedPhtos();
+
+// search photos
+const searchPhotos = async (query) => {
+  // authorizing the pexels api
+  const dataFetch = await fetch(`https://api.pexels.com/v1/search=${query}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: apiKey,
+    },
+  });
+  const searchData = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImages = document.createElement(`div`);
+    galleryImages.classList.add(`gallery-img`);
+    galleryImages.innerHTML = `<img src = ${photo.src.large}> </img> 
+    <p src=${photo.photographer}></p>`;
+    gallery.appendChild(galleryImages);
+  });
+};
